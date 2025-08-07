5cb4272432a359ea85af991e25562a7c8fface4b
mahliyoatanazarova <mahliyoatanazarova341@gmail.com>
2025-08-07 22:55:49 +0500

    kunuzz
    kunuzzkirish

---
+++ src/App.js
@@ -0,0 +1,53 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import './main.css';
+
+function App() {
+  return (
+    <div  id="div_in">
+              <img  classNomi="img1" src="./logo.png"/>
+    <a className='a1'> Sayt haqida</a>
+    <a className='a2'>RSS</a>
+    <a className='a3'> Aloqa</a>
+    <a className='a4'> Reklama</a>
+    <a className='a5'> Kun.uz jamoasi</a>
+    
+
+      <img src="./tglogo.png" alt="logo" className="img2" />
+      <img src="./facebl.webp" alt="logo" className="img3" />
+      <img src="./twitter.png" alt="logo" className="img4" />
+      <img src="./insta.png" alt="logo" className="img5" />
+      <img src="./appstore.png" alt="logo" className="img6" />
+      <img src="./google.png" alt="logo" className="img7" />
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
